9c0da1761653f9e0fac7de9b8b542946b415eba8
Pahaz Blinov <white.mathematician@gmail.com>
2014-07-11 00:08:44 +0600

    setting rename import

--- _project_/settings/section_mezzanine.py
+++ _project_/settings/section_mezzanine.py
@@ -1,29 +1,9 @@
-
 __author__ = 'pahaz'
 
 
 class Installer(object):
     @staticmethod
     def update_settings(g):
-
-        # The numeric mode to set newly-uploaded files to. The value should be
-        # a mode you'd pass directly to os.chmod.
-        g['FILE_UPLOAD_PERMISSIONS'] = 0o644
-
-        g['INSTALLED_APPS'] += (
-            "mezzanine.boot",
-            "mezzanine.conf",
-            "mezzanine.core",
-            "mezzanine.generic",
-            "mezzanine.blog",
-            "mezzanine.forms",
-            "mezzanine.pages",
-            "mezzanine.galleries",
-            "mezzanine.twitter",
-            # "mezzanine.accounts",
-            # "mezzanine.mobile",
-        )
-
         def append_if_not_exist(name, value):
             d = tuple(g[name])
             if value not in d:
@@ -48,8 +28,41 @@ class Installer(object):
                 d = d[:i] + (d[i], value) + d[i + 1:]
             g[name] = d
 
+        # The numeric mode to set newly-uploaded files to. The value should be
+        # a mode you'd pass directly to os.chmod.
+        g['FILE_UPLOAD_PERMISSIONS'] = 0o644
+
         g['AUTHENTICATION_BACKENDS'] = (
             "mezzanine.core.auth_backends.MezzanineBackend",)
+
+        # --------------------------------------------
+        # INSTALLED_APPS
+        # --------------------------------------------
+
+        INSTALLED_APPS = (
+            "django.contrib.admin",
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "django.contrib.redirects",
+            "django.contrib.sessions",
+            "django.contrib.sites",
+            "django.contrib.sitemaps",
+            "django.contrib.staticfiles",
+            "mezzanine.boot",
+            "mezzanine.conf",
+            "mezzanine.core",
+            "mezzanine.generic",
+            "mezzanine.blog",
+            "mezzanine.forms",
+            "mezzanine.pages",
+            "mezzanine.galleries",
+            "mezzanine.twitter",
+            #"mezzanine.accounts",
+            #"mezzanine.mobile",
+        )
+
+        for x in INSTALLED_APPS:
+            append_if_not_exist("INSTALLED_APPS", x)
 
         # --------------------------------------------
         # TEMPLATE_CONTEXT_PROCESSORS
@@ -136,77 +149,3 @@ class Installer(object):
             pass
         else:
             set_dynamic_settings(g)
-
-    @staticmethod
-    def update_urls(urlpatterns):
-        from django.conf.urls import patterns, include
-
-        urlpatterns += patterns('',
-
-            # We don't want to presume how your homepage works, so here are a
-            # few patterns you can use to set it up.
-
-            # HOMEPAGE AS STATIC TEMPLATE
-            # ---------------------------
-            # This pattern simply loads the index.html template. It isn't
-            # commented out like the others, so it's the default. You only need
-            # one homepage pattern, so if you use a different one, comment this
-            # one out.
-
-            #url("^$", direct_to_template, {"template": "index.html"}, name="home"),
-
-            # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
-            # ---------------------------------------------
-            # This pattern gives us a normal ``Page`` object, so that your
-            # homepage can be managed via the page tree in the admin. If you
-            # use this pattern, you'll need to create a page in the page tree,
-            # and specify its URL (in the Meta Data section) as "/", which
-            # is the value used below in the ``{"slug": "/"}`` part.
-            # Also note that the normal rule of adding a custom
-            # template per page with the template name using the page's slug
-            # doesn't apply here, since we can't have a template called
-            # "/.html" - so for this case, the template "pages/index.html"
-            # should be used if you want to customize the homepage's template.
-
-            # url("^$", "mezzanine.pages.views.page", {"slug": "/"}, name="home"),
-
-            # HOMEPAGE FOR A BLOG-ONLY SITE
-            # -----------------------------
-            # This pattern points the homepage to the blog post listing page,
-            # and is useful for sites that are primarily blogs. If you use this
-            # pattern, you'll also need to set BLOG_SLUG = "" in your
-            # ``settings.py`` module, and delete the blog page object from the
-            # page tree in the admin if it was installed.
-
-            # url("^$", "mezzanine.blog.views.blog_post_list", name="home"),
-
-            # MEZZANINE'S URLS
-            # ----------------
-            # ADD YOUR OWN URLPATTERNS *ABOVE* THE LINE BELOW.
-            # ``mezzanine.urls`` INCLUDES A *CATCH ALL* PATTERN
-            # FOR PAGES, SO URLPATTERNS ADDED BELOW ``mezzanine.urls``
-            # WILL NEVER BE MATCHED!
-
-            # If you'd like more granular control over the patterns in
-            # ``mezzanine.urls``, go right ahead and take the parts you want
-            # from it, and use them directly below instead of using
-            # ``mezzanine.urls``.
-            ("^", include("mezzanine.urls")),
-
-            # MOUNTING MEZZANINE UNDER A PREFIX
-            # ---------------------------------
-            # You can also mount all of Mezzanine's urlpatterns under a
-            # URL prefix if desired. When doing this, you need to define the
-            # ``SITE_PREFIX`` setting, which will contain the prefix. Eg:
-            # SITE_PREFIX = "my/site/prefix"
-            # For convenience, and to avoid repeating the prefix, use the
-            # commented out pattern below (commenting out the one above of course)
-            # which will make use of the ``SITE_PREFIX`` setting. Make sure to
-            # add the import ``from django.conf import settings`` to the top
-            # of this file as well.
-            # Note that for any of the various homepage patterns above, you'll
-            # need to use the ``SITE_PREFIX`` setting as well.
-
-            # ("^%s/" % settings.SITE_PREFIX, include("mezzanine.urls"))
-
-        )
