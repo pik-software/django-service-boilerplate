ee5771f5d200b0893b05a7ae463200c7b1e2c1f5
Pahaz Blinov <white.mathematician@gmail.com>
2014-07-11 00:12:08 +0600

    fix urls

--- _project_/urls.py
+++ _project_/urls.py
@@ -10,14 +10,13 @@ admin.autodiscover()
 
 urlpatterns = i18n_patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
+    # url name `home` required for mezzanine
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 
     # url(r'^app/', include("app.urls", namespace="app")),
     # use section urls if need!
 )
-
-
 
 if settings.DEBUG:
     from django.conf.urls.static import static
