f182d9e472597b299be89b00d5f427eeafde5f8a
Pahaz Blinov <white.mathematician@gmail.com>
2014-07-11 19:59:15 +0600

    fix test.py

--- test.py
+++ test.py
@@ -13,11 +13,18 @@ def venv_activate_command():
     return active if sys.platform == 'win32' else 'source ' + active
 
 
-if not os.path.exists('.test'):
-    os.mkdir('mkdir .test')
+def vpython_bin():
+    bin = 'Scripts' if sys.platform == 'win32' else 'bin'
+    py = join(_root, '__data__', 'venv', bin, 'python')
+    return py
+
+
+tests_dir = join(_root, '.test')
+if not os.path.exists(tests_dir):
+    print("#!# os.mkdir! .test")
+    os.mkdir(tests_dir)
 
 exec_file = join(_root, '.test', 'go.sh.cmd')
-
 f = open(exec_file, 'w')
 f.write("""#!/bin/bash
 pushd "%~dp0"
@@ -26,11 +33,13 @@ git clone https://github.com/pahaz/django-project-stub.git project-name
 cd project-name
 python setup.py
 python helpers/mkvirtualenv.py
-{venv_activate_command}
-python manage.py syncdb --noinput
-python manage.py runserver
+{vpython_bin} manage.py syncdb --noinput
+{vpython_bin} manage.py runserver
 popd
-""".format(venv_activate_command=venv_activate_command()))
+""".format(
+    venv_activate_command=venv_activate_command(),
+    vpython_bin=vpython_bin(),
+))
 f.close()
 
 print(exec_file)
