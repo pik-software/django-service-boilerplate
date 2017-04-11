d862008d962eaca81e1fc0556f84110ce1e414e8
pahaz <pahaz.blinov@gmail.com>
2017-04-12 02:41:21 +0500

    add selenium based tests

--- _project_/tests/test_admin.py
+++ _project_/tests/test_admin.py
@@ -1,3 +1,3 @@
-def test_admin_index_view(admin_client):
+def test_admin_index(admin_client):
     response = admin_client.get('/admin/')
     assert response.status_code == 200
--- conftest.py
+++ conftest.py
@@ -1,8 +1,38 @@
 import os
 
+from selenium import webdriver
+import pytest
 import django
 
 
 def pytest_configure():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_project_.settings")
     django.setup()
+
+
+@pytest.fixture(scope='session')
+def base_url(live_server):
+    return live_server.url
+
+
+@pytest.fixture
+def driver_class(request):
+	return webdriver.Firefox
+
+
+@pytest.fixture
+def driver_kwargs():
+	return {}
+
+
+@pytest.yield_fixture
+def driver(request, driver_class, driver_kwargs):
+    """Returns a WebDriver instance based on options and capabilities"""
+    driver = driver_class(**driver_kwargs)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def _skip_sensitive(request):
+    """Don't Skip destructive tests"""
