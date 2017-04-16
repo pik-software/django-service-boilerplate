d5a709e1998037dde20055be58fec3a8c6bb300d
pahaz <pahaz.blinov@gmail.com>
2017-04-16 15:58:31 +0500

    serving static from django

--- _project_/urls.py
+++ _project_/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""_project_ URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 
 
 def index(request):
@@ -28,10 +29,11 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
+urlpatterns += static(
+    settings.STATIC_URL, 
+    document_root=settings.STATIC_ROOT
+)
