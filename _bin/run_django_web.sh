1a4d2793b443f55d65156b1d20d9129a878517e8
pahaz <pahaz.blinov@gmail.com>
2017-04-16 14:41:06 +0500

    fix uwsgi

--- _bin/run_django_web.sh
+++ _bin/run_django_web.sh
@@ -3,4 +3,4 @@
 nprocessors=$(($(nproc) * 2))
 
 #python manage.py migrate
-exec uwsgi --module _project_.wsgi --processes $nprocessors --http :80 --http-to /tmp/uwsgi.sock --master --die-on-term --enable-threads --single-interpreter --wsgi-disable-file-wrapper --limit-post 4294967296
+exec uwsgi --module _project_.wsgi --processes $nprocessors --http :8000 --http-to /tmp/uwsgi.sock --master --die-on-term --enable-threads --single-interpreter --wsgi-disable-file-wrapper --limit-post 4294967296
