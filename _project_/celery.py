4f0d07282c22343de1b793a0f2ccd076875de402
pahaz <pahaz.blinov@gmail.com>
2017-04-16 12:21:15 +0500

    celery: refactoring

--- _project_/celery.py
+++ _project_/celery.py
@@ -1,22 +1,22 @@
-from __future__ import absolute_import
-
+from __future__ import absolute_import, unicode_literals
 import os
-
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_project_.settings')
 
-from django.conf import settings  # noqa
-
 app = Celery('_project_')
 
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# Using a string here means the worker don't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
 
 
 @app.task(bind=True)
 def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+    return 'Request: {0!r}'.format(self.request.task)
