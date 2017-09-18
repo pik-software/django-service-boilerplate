048d4e1342c118238aa0a74f0156a2da54a69029
pahaz <pahaz.blinov@gmail.com>
2017-09-18 14:21:29 +0300

    urls.py: cleaning

--- _project_/urls.py
+++ _project_/urls.py
@@ -34,6 +34,6 @@ urlpatterns += static(
     document_root=settings.MEDIA_ROOT
 )
 urlpatterns += static(
-    settings.STATIC_URL, 
+    settings.STATIC_URL,
     document_root=settings.STATIC_ROOT
 )
