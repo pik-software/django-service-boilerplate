257309f846e7fa88d3186957a5ba082b322e3676
Pahaz White <pahaz.white@gmail.com>
2017-11-22 15:19:06 +0300

    settings.py: add django.contrib.postgres

--- _project_/settings.py
+++ _project_/settings.py
@@ -87,6 +87,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'django.contrib.gis',
     'rest_framework',
 
