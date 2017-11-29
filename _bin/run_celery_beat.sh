df67f43f53251b2dc37914da8f40b832492a33df
Pahaz White <pahaz.white@gmail.com>
2017-11-30 01:26:03 +0300

    run_celery_beat.sh: fix permission problem

--- _bin/run_celery_beat.sh
+++ _bin/run_celery_beat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec python -u -m celery beat -A _project_ --loglevel info
+exec python -u -m celery beat -A _project_ --loglevel info --pidfile="/tmp/celerybeat.pid" --schedule="/tmp/celerybeat-schedule.db"
