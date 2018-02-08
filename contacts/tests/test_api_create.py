9b026896235ac3d30827ff75d007bf69c94740b6
Pahaz White <pahaz.white@gmail.com>
2018-02-08 03:23:15 +0300

    core.api: standardise errors

--- contacts/tests/test_api_create.py
+++ contacts/tests/test_api_create.py
@@ -35,3 +35,20 @@ def test_api_create_bulk(logged_user_client):
     res = logged_user_client.post('/api/v1/contact-list/', data=data)
     assert res.status_code == status.HTTP_201_CREATED
     assert len(res.data) == 2
+
+
+def test_api_create_without_name(logged_user_client):
+    data = {'noname': get_random_string()}
+    res = logged_user_client.post('/api/v1/contact-list/', data=data)
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert res.data == {
+        'code': 'invalid',
+        'detail': {
+            'name': [{'code': 'required', 'message': 'Это поле обязательно.'}]},
+        'message': 'Invalid input.'}
+
+
+def test_api_create_with_extra_field(logged_user_client):
+    data = {'name': get_random_string(), 'fooo': 'no'}
+    res = logged_user_client.post('/api/v1/contact-list/', data=data)
+    assert res.status_code == status.HTTP_201_CREATED
