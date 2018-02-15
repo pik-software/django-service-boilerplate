ba1909fedc5b1600378c5873a75695d2ddb2cc53
Pahaz White <pahaz.white@gmail.com>
2018-02-16 01:10:54 +0300

    CircleCI: run generate_staging_data.py

---
+++ _bin/generate_staging_data.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+import django
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+def django_setup():
+    sys.path.insert(0, BASE_DIR)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_project_.settings")
+    django.setup()
+
+
+def after_django_setup():
+    from django.contrib.auth.models import User  # noqa
+    from contacts.tests.factories import *  # noqa
+
+    user, _ = User.objects.get_or_create(username='staging-superuser')
+    user.is_staff = True
+    user.is_superuser = True
+    user.set_password('AwfnJAWkfjnawfkNAKA@(Afw1w')
+    user.save()
+
+    ContactFactory.create_batch(2000)
+
+
+if __name__ == '__main__':
+    django_setup()
+    after_django_setup()
