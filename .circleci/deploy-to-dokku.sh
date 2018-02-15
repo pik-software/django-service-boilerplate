cdd8c9a9b1bd160f3f1d07e2125b3b5f8804b88a
Pahaz White <pahaz.white@gmail.com>
2018-02-16 01:23:26 +0300

    CircleCI: fix generate_staging_data.py

--- .circleci/deploy-to-dokku.sh
+++ .circleci/deploy-to-dokku.sh
@@ -6,3 +6,4 @@ BRANCH=$2
 git push ssh://dokku@staging.pik-software.ru/${REPO}-${BRANCH} ${BRANCH}:master
 ssh dokku@staging.pik-software.ru -C "run ${REPO}-${BRANCH} python manage.py migrate"
 ssh dokku@staging.pik-software.ru -C "run ${REPO}-${BRANCH} python _bin/generate_staging_data.py"
+echo "open !!! http://${REPO}-${BRANCH}.pik-software.ru/"
