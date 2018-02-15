8f26a52480bdfba777ea3f07a7d097f89c8b792a
Pahaz White <pahaz.white@gmail.com>
2018-02-16 00:53:59 +0300

    CircleCI: run migrations

--- .circleci/deploy-to-dokku.sh
+++ .circleci/deploy-to-dokku.sh
@@ -4,3 +4,4 @@ REPO=$1
 BRANCH=$2
 
 git push ssh://dokku@staging.pik-software.ru/${REPO}-${BRANCH} ${BRANCH}:master
+ssh dokku@staging.pik-software.ru -C "run ${REPO}-${BRANCH} python manage.py migrate"
