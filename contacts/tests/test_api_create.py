4ef679f26e16b66c0af408915dcd94a3df4f44c0
Pahaz White <pahaz.white@gmail.com>
2018-02-18 21:35:36 +0300

    API: PrimaryKeyModelSerializerField with examples and tests

--- contacts/tests/test_api_create.py
+++ contacts/tests/test_api_create.py
@@ -1,10 +1,15 @@
 import django.test
 import pytest
+from django.contrib.contenttypes.models import ContentType
 from django.utils.crypto import get_random_string
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from contacts.tests.factories import ContactFactory
 from core.tasks.fixtures import create_user
+
+
+REQUIRED_FIELD_ERROR = {'message': 'Это поле обязательно.', 'code': 'required'}
 
 
 @pytest.fixture()
@@ -16,6 +21,7 @@ def client():
 def logged_user_client(client: django.test.Client):
     user = create_user()
     client.force_login(user)
+    client.user = user
     return client
 
 
@@ -24,31 +30,119 @@ def test_api_authorization(client):
     assert res.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-def test_api_create(logged_user_client):
+def test_api_contact_create_without_request_user(client):
+    data = {'name': get_random_string()}
+    res = client.post('/api/v1/contact-list/', data=data)
+    assert res.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_api_contact_create(logged_user_client):
     data = {'name': get_random_string()}
     res = logged_user_client.post('/api/v1/contact-list/', data=data)
     assert res.status_code == status.HTTP_201_CREATED
 
 
-def test_api_create_bulk(logged_user_client):
+def test_api_contact_create_bulk(logged_user_client):
     data = [{'name': get_random_string()}, {'name': get_random_string()}]
     res = logged_user_client.post('/api/v1/contact-list/', data=data)
     assert res.status_code == status.HTTP_201_CREATED
     assert len(res.data) == 2
 
 
-def test_api_create_without_name(logged_user_client):
+def test_api_contact_create_without_name(logged_user_client):
     data = {'noname': get_random_string()}
     res = logged_user_client.post('/api/v1/contact-list/', data=data)
     assert res.status_code == status.HTTP_400_BAD_REQUEST
     assert res.data == {
         'code': 'invalid',
         'detail': {
-            'name': [{'code': 'required', 'message': 'Это поле обязательно.'}]},
+            'name': [REQUIRED_FIELD_ERROR]},
         'message': 'Invalid input.'}
 
 
-def test_api_create_with_extra_field(logged_user_client):
+def test_api_contact_create_with_extra_field(logged_user_client):
     data = {'name': get_random_string(), 'fooo': 'no'}
     res = logged_user_client.post('/api/v1/contact-list/', data=data)
     assert res.status_code == status.HTTP_201_CREATED
+
+
+def test_api_comment_create_without_request_user(client):
+    data = {'message': get_random_string()}
+    res = client.post('/api/v1/comment-list/', data=data)
+    assert res.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_api_comment_create_without_user(logged_user_client):
+    data = {'message': get_random_string()}
+    res = logged_user_client.post('/api/v1/comment-list/', data=data)
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert res.data == {
+        'message': 'Invalid input.', 'code': 'invalid',
+        'detail': {
+            'contact': [REQUIRED_FIELD_ERROR],
+        }
+    }
+
+
+def test_api_comment_create(logged_user_client):
+    contact = ContactFactory.create()
+    payload = {
+        '_uid': contact.uid,
+        '_type': ContentType.objects.get_for_model(type(contact)).model,
+        'name': get_random_string(),
+    }
+
+    data = {'message': get_random_string(), 'contact': payload}
+    res = logged_user_client.post('/api/v1/comment-list/', data=data)
+    assert res.status_code == status.HTTP_201_CREATED
+    assert res.data == {
+        '_uid': res.data['_uid'],
+        '_type': 'comment',
+        'user': logged_user_client.user.pk,
+        'message': data['message'],
+        'contact': {
+            '_uid': contact.uid,
+            '_type': 'contact',
+            'name': contact.name,
+            'phones': contact.phones,
+            'emails': contact.emails,
+            'order_index': contact.order_index,
+        },
+    }
+
+
+def test_api_comment_create_simple(logged_user_client):
+    contact = ContactFactory.create()
+    data = {'message': get_random_string(), 'contact': contact.uid}
+    res = logged_user_client.post('/api/v1/comment-list/', data=data)
+    assert res.status_code == status.HTTP_201_CREATED
+    assert res.data == {
+        '_uid': res.data['_uid'],
+        '_type': 'comment',
+        'user': logged_user_client.user.pk,
+        'message': data['message'],
+        'contact': {
+            '_uid': contact.uid,
+            '_type': 'contact',
+            'name': contact.name,
+            'phones': contact.phones,
+            'emails': contact.emails,
+            'order_index': contact.order_index,
+        },
+    }
+
+
+def test_api_comment_create_2_comments_for_one_contact(logged_user_client):
+    contact = ContactFactory.create()
+    payload = {
+        '_uid': contact.uid,
+        '_type': ContentType.objects.get_for_model(type(contact)).model,
+    }
+
+    data = [
+        {'message': get_random_string(), 'contact': payload},
+        {'message': get_random_string(), 'contact': payload},]
+    res = logged_user_client.post('/api/v1/comment-list/', data=data)
+    assert res.status_code == status.HTTP_201_CREATED
+    assert len(res.data) == 2
+    assert contact.comments.all().count() == 2
---
+++ core/api/fields.py
@@ -0,0 +1,151 @@
+from collections import OrderedDict
+
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import ugettext as _
+from django.db.models import FieldDoesNotExist
+from rest_framework import serializers
+from rest_framework.fields import empty
+
+from ..api.serializers import StandardizedModelSerializer
+
+
+def _get_model_field(model, field):
+    try:
+        return model._meta.get_field(field)  # noqa
+    except FieldDoesNotExist:
+        return None
+
+
+class PrimaryKeyModelSerializerField(serializers.Field):
+    """
+    Use it if you want to use ModelSerializer inside other ModelSerializer.
+
+    Use `allowed_objects` function to restrict access to nested objects
+    for some users. To check access during creation and modification
+    of nested objects.
+
+    Example:
+
+        def allow_all_objects(serializer) -> QuerySet:
+            return serializer.Meta.model.objects.all()
+
+        def allow_only_user_objects(serializer) -> QuerySet:
+            return serializer.Meta.model.objects.all().filter(
+                user=serializer.context['request'].user)
+
+        class ContactSerializer(StandardizedModelSerializer):
+            class Meta:
+                model = Contact
+                read_only_fields = ('_uid', '_type', 'user')
+                fields = ('_uid', '_type', 'user', 'name')
+
+        class CommentSerializer(StandardizedModelSerializer):
+            contact = PrimaryKeyModelSerializerField(
+                ContactSerializer,
+                allowed_objects=allow_only_user_objects,  # !!
+            )
+
+            class Meta:
+                model = Comment
+                read_only_fields = ('_uid', '_type', 'user')
+                fields = ('_uid', '_type', 'user', 'contact', 'message')
+    """
+    default_error_messages = {
+        'required': _(
+            'This field is required.'),
+        'does_not_exist': _(
+            'Invalid pk "{pk_value}" - object does not exist.'),
+        'incorrect_type': _(
+            'Incorrect type. Expected pk value, received {data_type}.'),
+        'no_uid': _('No _uid value'),
+        'no_type': _('No _type value'),
+        'incorrect_type_value': _('Invalid _type value, expect {data_type}'),
+    }
+
+    def __init__(self, serializer, **kwargs):
+        if not issubclass(serializer, StandardizedModelSerializer):
+            raise TypeError(
+                "serializer argument should be a StandardizedModelSerializer "
+                "instance")
+
+        self._model = model = getattr(serializer, 'Meta').model
+        if _get_model_field(model, 'uid'):
+            pk = serializers.UUIDField(format='hex')
+            pk.field_name = 'uid'
+        else:
+            # If you really want, You can add pk_field argument (similar to
+            # rest_framework.serializers.PrimaryKeyRelatedField argument).
+            # At the moment, I do not see this as an urgent need.
+            raise TypeError("unknown pk_field for model")
+
+        get_allowed_objects = kwargs.pop('allowed_objects', None)
+        self._get_allowed_objects = get_allowed_objects
+        if get_allowed_objects and not callable(get_allowed_objects):
+            raise TypeError("get_allowed_objects argument should be callable")
+
+        self._pk_field = pk
+        self._serializer = serializer
+
+        has_get_allowed_objects = bool(self._get_allowed_objects)
+        is_read_only = kwargs.get('read_only', None)
+        is_many = kwargs.get('many', None)
+        assert isinstance(self._pk_field.field_name, str), (
+            'no pk_field.field_name'
+        )
+        assert not is_many, (
+            "PrimaryKeyNestedModelSerializerField fields should "
+            "not set many=True"
+        )
+        assert has_get_allowed_objects or is_read_only, (
+            'PrimaryKeyNestedModelSerializerField field must provide '
+            'a `allowed_objects` callable argument or set read_only=`True`.'
+        )
+        super().__init__(**kwargs)
+
+    def _get_serializer(self, instance=None, data=empty):
+        assert self.parent, (
+            "you should use PrimaryKeyNestedModelSerializerField "
+            "inside StandardizedModelSerializer"
+        )
+        assert self.parent.context.get('request'), (
+            "no self.parent.context.get('request'). "
+            "you should use PrimaryKeyNestedModelSerializerField "
+            "inside StandardizedModelSerializer"
+        )
+        return self._serializer(
+            instance=instance, data=data, context=self.parent.context)
+
+    def bind(self, field_name, parent):
+        # just assert it!
+        assert isinstance(parent, StandardizedModelSerializer), (
+            "you should use PrimaryKeyNestedModelSerializerField "
+            "inside StandardizedModelSerializer"
+        )
+        return super().bind(field_name, parent)
+
+    def to_internal_value(self, data):
+        if isinstance(data, (dict, OrderedDict)):
+            _uid = data.get('_uid')
+            _type = data.get('_type')
+            if not _uid:
+                self.fail('no_uid')
+            if not _type:
+                self.fail('no_type')
+            model_type = ContentType.objects.get_for_model(self._model).model
+            if _type != model_type:
+                self.fail('incorrect_type_value', data_type=model_type)
+            data = _uid
+        try:
+            data = self._pk_field.to_internal_value(data)
+        except (TypeError, ValueError):
+            self.fail('incorrect_type', data_type=type(data).__name__)
+        try:
+            serializer = self._get_serializer()
+            return self._get_allowed_objects(serializer).get(**{
+                self._pk_field.field_name: data})
+        except ObjectDoesNotExist:
+            self.fail('does_not_exist', pk_value=data)
+
+    def to_representation(self, value):
+        return self._get_serializer(value).data
