8ea40e264b31ecfe0ca883af2efc24d5a5494620
Pahaz White <pahaz.white@gmail.com>
2018-02-19 00:14:04 +0300

    tests: num queries

--- contacts/tests/test_api_standardised.py
+++ contacts/tests/test_api_standardised.py
@@ -8,12 +8,12 @@ from ..models import Contact, Comment
 from ..tests.factories import ContactFactory, CommentFactory
 
 
-BATCH_MODELS = 3
+BATCH_MODELS = 5
 
 
 @pytest.fixture(params=[
-    (Contact, ContactFactory, 1),
-    (Comment, CommentFactory, 1),
+    (Contact, ContactFactory, {'version': 1, 'queries': BATCH_MODELS}),
+    (Comment, CommentFactory, {'version': 1, 'queries': BATCH_MODELS}),
 ])
 def api_model(request):
     return request.param
@@ -29,13 +29,13 @@ def api_client():
 
 
 def test_api_list(api_client, api_model):
-    model, factory, version = api_model
+    model, factory, options = api_model
     factory.create_batch(BATCH_MODELS)
     last_obj = factory.create()
 
     _type = ContentType.objects.get_for_model(model).model
 
-    url = f'/api/v{version}/{_type}-list/'
+    url = f'/api/v{options["version"]}/{_type}-list/'
     res = api_client.get(url)
     assert res.status_code == status.HTTP_200_OK
     assert res.data['count'] > BATCH_MODELS
@@ -46,17 +46,34 @@ def test_api_list(api_client, api_model):
     assert res.data['page_previous'] is None
     assert res.data['results'][0]['_uid'] == last_obj.uid
     assert res.data['results'][0]['_type'] == _type
+    assert len(res.data['results']) > BATCH_MODELS
 
 
 def test_api_retrieve(api_client, api_model):
-    model, factory, version = api_model
+    model, factory, options = api_model
     factory.create_batch(BATCH_MODELS)
     last_obj = factory.create()
 
     _type = ContentType.objects.get_for_model(model).model
 
-    url = f'/api/v{version}/{_type}-list/{last_obj.uid}/'
+    url = f'/api/v{options["version"]}/{_type}-list/{last_obj.uid}/'
     res = api_client.get(url)
     assert res.status_code == status.HTTP_200_OK
     assert res.data['_uid'] == last_obj.uid
     assert res.data['_type'] == _type
+
+
+def test_api_list_num_queries(
+        api_client, api_model,
+        assert_num_queries_lte
+):
+    model, factory, options = api_model
+    factory.create_batch(BATCH_MODELS)
+    _type = ContentType.objects.get_for_model(model).model
+
+    url = f'/api/v{options["version"]}/{_type}-list/'
+
+    with assert_num_queries_lte(options["queries"]):
+        res = api_client.get(url)
+    assert res.status_code == status.HTTP_200_OK
+    assert len(res.data['results']) >= BATCH_MODELS
