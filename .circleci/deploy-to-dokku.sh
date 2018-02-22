aad90aaac90cc5638d47a7f19f5881fd695a7600
Pahaz White <pahaz.white@gmail.com>
2018-02-23 01:42:45 +0500

    SHOW DEPLOY ERRORS

--- .circleci/deploy-to-dokku.sh
+++ .circleci/deploy-to-dokku.sh
@@ -4,7 +4,8 @@ set -x
 REPO=$1
 BRANCH=$2
 
-git push ssh://dokku@staging.pik-software.ru/${REPO}-${BRANCH} ${BRANCH}:master
-ssh dokku@staging.pik-software.ru -C "run ${REPO}-${BRANCH} python manage.py migrate"
-ssh dokku@staging.pik-software.ru -C "run ${REPO}-${BRANCH} python _bin/generate_staging_data.py"
-echo "open !!! http://${REPO}-${BRANCH}.pik-software.ru/"
+echo "DEPLOY !!! http://${REPO}-${BRANCH}.pik-software.ru/"
+git push ssh://dokku@staging.pik-software.ru/${REPO}-${BRANCH} ${BRANCH}:master || exit 1
+ssh dokku@staging.pik-software.ru -C "run ${REPO}-${BRANCH} python manage.py migrate" || exit 2
+ssh dokku@staging.pik-software.ru -C "run ${REPO}-${BRANCH} python _bin/generate_staging_data.py" || exit 3
+echo "OPEN FRESH !!! http://${REPO}-${BRANCH}.pik-software.ru/"
