c4a35d76eb0600c20389c6106e5a9053bc10e3ee
martbln <stepinilya@gmail.com>
2018-03-20 09:21:38 +0300

    delete migration files

--- core/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig as BaseAppConfig
-from django.utils.translation import ugettext_lazy as _
-
-
-class AppConfig(BaseAppConfig):
-    name = 'core'
-    verbose_name = _('Утилиты')
