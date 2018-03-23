d420925e515fc965d97c8dea30faae9dd35713d8
Alexander Klimenko <alex@erix.ru>
2018-03-23 08:59:21 +0300

    depploy-to-dokku: add deployment REPO & BRANCH autodetection

--- .circleci/deploy-to-dokku.sh
+++ .circleci/deploy-to-dokku.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -x
 
-REPO=$1
-BRANCH=$2
+REPO=`git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p'`
+BRANCH=`git branch | grep -e "^*" | cut -d' ' -f 2`
 
-git push ssh://dokku@staging.pik-software.ru/${REPO}-${BRANCH} ${BRANCH}:master
-ssh dokku@staging.pik-software.ru -C "run ${REPO}-${BRANCH} python manage.py migrate"
-ssh dokku@staging.pik-software.ru -C "run ${REPO}-${BRANCH} python _bin/generate_staging_data.py"
-echo "open !!! http://${REPO}-${BRANCH}.pik-software.ru/"
+echo "DEPLOY !!! http://${REPO}-${BRANCH}.pik-software.ru/"
+git push ssh://dokku@staging.pik-software.ru/${REPO}-${BRANCH} ${BRANCH}:master || exit 1
+ssh dokku@staging.pik-software.ru -C "run ${REPO}-${BRANCH} python manage.py migrate" || exit 2
+echo "OPEN FRESH !!! http://${REPO}-${BRANCH}.pik-software.ru/"
