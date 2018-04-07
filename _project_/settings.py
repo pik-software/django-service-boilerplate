bac696bf3ac91e06ce8d99a0796fba939588ad9a
Pahaz White <pahaz.white@gmail.com>
2018-04-07 16:35:03 +0300

    settings.py: typo

--- _project_/settings.py
+++ _project_/settings.py
@@ -261,7 +261,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication'
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
