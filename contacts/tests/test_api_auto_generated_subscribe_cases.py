4b7e67174651af9d71f0eeecd24c1660441d4e43
Pahaz White <pahaz.white@gmail.com>
2018-04-30 00:34:46 +0500

    replication: test_api_auto_generated_subscribe_cases.py v0

--- contacts/tests/test_api_auto_generated_subscribe_cases.py
+++ contacts/tests/test_api_auto_generated_subscribe_cases.py
@@ -63,9 +63,11 @@ def _create_subscribe_permission(user, model):
 
 
 def _create_history_permission(user, model):
-    model_type_name = ContentType.objects.get_for_model(model).model
-    permission = Permission.objects.get(
-        codename=f'can_get_api_{model_type_name}_history')
+    opts = model._meta  # noqa: pylint=protected-access
+    content_type = ContentType.objects.get_for_model(model)
+    permission, _ = Permission.objects.get_or_create(
+        content_type=content_type,
+        codename=f'view_historical{opts.model_name}')
     user.user_permissions.add(permission)
 
 
@@ -101,177 +103,177 @@ def _get_history_content(model, options, user, hist_obj):
     assert status == 200
     return content
 
-#
-# def test_api_subscribe_access_denied(api_client, api_model):
-#     model, factory, options = api_model
-#     _create_few_models(factory)
-#     url = _url(model, options)
-#
-#     res = api_client.post(url, {"name": "test"})
-#
-#     assert res.status_code == status.HTTP_403_FORBIDDEN
-#     assert res.data == {
-#         'code': 'permission_denied', 'message': 'Access denied'}
-#
-#
-# def test_api_subscribe(api_client, api_model):
-#     model, factory, options = api_model
-#     url = _url(model, options)
-#     _type = ContentType.objects.get_for_model(model).model
-#
-#     _create_subscribe_permission(api_client.user, model)
-#     res = api_client.post(url, {
-#         'name': 'test', 'type': 1, 'settings': {
-#             'webhook_url': 'http://localhost/',
-#         },
-#     })
-#
-#     obj = Subscribe.objects.last()
-#
-#     print(res.data)
-#     assert res.status_code == status.HTTP_200_OK
-#     assert res.data == {
-#         '_uid': obj.uid,
-#         '_type': 'subscribe',
-#         'events': [_type],
-#         'type': 1,
-#         'name': 'test',
-#         'settings': {
-#             'api_version': options['version'],
-#             'webhook_url': 'http://localhost/'},
-#     }
-#
-#
-# def test_api_subscribe_events(api_model, mocker):
-#     model, factory, options = api_model
-#     _create_subscribe(model, options)
-#     _type = ContentType.objects.get_for_model(model).model
-#     task = mocker.patch('replication.tasks.replicate_history')
-#
-#     obj = factory.create()
-#     history = obj.history.all()
-#     _uid = obj.pk
-#
-#     hist_obj = history.first()
-#
-#     # create event
-#     _assert_history_object(hist_obj, _type, '+', _uid)
-#     task.delay.assert_called_once_with(
-#         hist_obj._meta.app_label, hist_obj._meta.model_name, hist_obj.pk,
-#     )
-#
-#     # change event
-#     obj.version += 10
-#     obj.save()
-#     hist_obj = history.first()
-#
-#     _assert_history_object(hist_obj, _type, '~', _uid)
-#     task.delay.assert_called_with(
-#         hist_obj._meta.app_label, hist_obj._meta.model_name, hist_obj.pk,
-#     )
-#
-#     # delete event
-#     obj.delete()
-#     hist_obj = history.first()
-#
-#     _assert_history_object(hist_obj, _type, '-', _uid)
-#     task.delay.assert_called_with(
-#         hist_obj._meta.app_label, hist_obj._meta.model_name, hist_obj.pk,
-#     )
-#
-#
-# def test_do_async_fake_request(api_model):
-#     model, factory, options = api_model
-#     user = create_user()
-#     _create_history_permission(user, model)
-#     _type = ContentType.objects.get_for_model(model).model
-#     obj = _create_few_models(factory)
-#     hist_obj = obj.history.first()
-#     history_url = f'/api/v{options["version"]}/{_type}-list/history/'
-#
-#     code, content = _do_async_fake_request(
-#         user.pk, 'get', history_url, data={'history_id': hist_obj.history_id})
-#
-#     assert code == 200
-#
-#     content_json = json.loads(content)
-#     assert content_json['count'] == 1
-#     assert content_json['results'][0]["history_type"] == '+'
-#     assert content_json['results'][0]["history_id"] == hist_obj.history_id
-#     assert content_json['results'][0]["_uid"] == str(hist_obj.uid)
-#     assert content_json['results'][0]["_type"] == 'historical' + _type
-#
-#
-# def test_replicate_history_call_process_webhook(
-#         api_model, mocker, celery_session_worker):
-#     model, factory, options = api_model
-#     subscribe = _create_subscribe(model, options)
-#     process_webhook = mocker.patch(
-#         'replication.tasks._replicate_history_process_webhook_subscriber')
-#
-#     # create event
-#     obj = factory.create()
-#     history = obj.history.all()
-#     hist1 = history.first()
-#     app_label, model_name = hist1._meta.app_label, hist1._meta.model_name
-#
-#     r = replicate_history.delay(app_label, model_name, hist1.pk)
-#     r.get(timeout=10)
-#     assert process_webhook.delay.call_args_list == [
-#         call(subscribe.pk, app_label, model_name, hist1.pk),
-#         call(subscribe.pk, app_label, model_name, hist1.pk),
-#     ]
-#
-#     # change event
-#     obj.version += 10
-#     obj.save()
-#     hist2 = history.first()
-#
-#     r = replicate_history.delay(app_label, model_name, hist2.pk)
-#     r.get(timeout=10)
-#     assert process_webhook.delay.call_args_list == [
-#         call(subscribe.pk, app_label, model_name, hist1.pk),
-#         call(subscribe.pk, app_label, model_name, hist1.pk),
-#         call(subscribe.pk, app_label, model_name, hist2.pk),
-#         call(subscribe.pk, app_label, model_name, hist2.pk),
-#     ]
-#
-#     # delete event
-#     obj.delete()
-#     hist3 = history.first()
-#
-#     r = replicate_history.delay(app_label, model_name, hist3.pk)
-#     r.get(timeout=10)
-#     assert process_webhook.delay.call_args_list == [
-#         call(subscribe.pk, app_label, model_name, hist1.pk),
-#         call(subscribe.pk, app_label, model_name, hist1.pk),
-#         call(subscribe.pk, app_label, model_name, hist2.pk),
-#         call(subscribe.pk, app_label, model_name, hist2.pk),
-#         call(subscribe.pk, app_label, model_name, hist3.pk),
-#         call(subscribe.pk, app_label, model_name, hist3.pk),
-#     ]
 
-#
-# def test_process_webhook(api_model, mocker, celery_session_worker):
-#     model, factory, options = api_model
-#     subscribe = _create_subscribe(model, options)
-#     process_webhook = _replicate_history_process_webhook_subscriber
-#     transfer = mocker.patch('replication.tasks._transfer_webhook_data')
-#
-#     # create event
-#     obj = _create_few_models(factory)
-#     history = obj.history.all()
-#     hist1 = history.first()
-#     hist1_content = _get_history_content(model, options, subscribe.user, hist1)
-#     app_label, model_name = hist1._meta.app_label, hist1._meta.model_name
-#
-#     r = process_webhook.delay(subscribe.pk, app_label, model_name, hist1.pk)
-#     assert r.get(timeout=10) == 'ok'
-#     assert transfer.call_args_list == [
-#         call('http://localhost/', hist1_content),
-#         call('http://localhost/', hist1_content),
-#     ]
-#
+def test_api_subscribe_access_denied(api_client, api_model):
+    model, factory, options = api_model
+    _create_few_models(factory)
+    url = _url(model, options)
+
+    res = api_client.post(url, {"name": "test"})
+
+    assert res.status_code == status.HTTP_403_FORBIDDEN
+    assert res.data == {
+        'code': 'permission_denied', 'message': 'Access denied'}
+
+
+def test_api_subscribe(api_client, api_model):
+    model, factory, options = api_model
+    url = _url(model, options)
+    _type = ContentType.objects.get_for_model(model).model
+
+    _create_subscribe_permission(api_client.user, model)
+    res = api_client.post(url, {
+        'name': 'test', 'type': 1, 'settings': {
+            'webhook_url': 'http://localhost/',
+        },
+    })
+
+    obj = Subscribe.objects.last()
+
+    print(res.data)
+    assert res.status_code == status.HTTP_200_OK
+    assert res.data == {
+        '_uid': obj.uid,
+        '_type': 'subscribe',
+        'events': [_type],
+        'type': 1,
+        'name': 'test',
+        'settings': {
+            'api_version': options['version'],
+            'webhook_url': 'http://localhost/'},
+    }
+
+
+def test_api_subscribe_events(api_model, mocker):
+    model, factory, options = api_model
+    _create_subscribe(model, options)
+    _type = ContentType.objects.get_for_model(model).model
+    task = mocker.patch('replication.tasks.replicate_history')
+
+    obj = factory.create()
+    history = obj.history.all()
+    _uid = obj.pk
+
+    hist_obj = history.first()
+
+    # create event
+    _assert_history_object(hist_obj, _type, '+', _uid)
+    task.delay.assert_called_once_with(
+        hist_obj._meta.app_label, hist_obj._meta.model_name, hist_obj.pk,
+    )
+
+    # change event
+    obj.version += 10
+    obj.save()
+    hist_obj = history.first()
+
+    _assert_history_object(hist_obj, _type, '~', _uid)
+    task.delay.assert_called_with(
+        hist_obj._meta.app_label, hist_obj._meta.model_name, hist_obj.pk,
+    )
+
+    # delete event
+    obj.delete()
+    hist_obj = history.first()
+
+    _assert_history_object(hist_obj, _type, '-', _uid)
+    task.delay.assert_called_with(
+        hist_obj._meta.app_label, hist_obj._meta.model_name, hist_obj.pk,
+    )
+
+
+def test_do_async_fake_request(api_model):
+    model, factory, options = api_model
+    user = create_user()
+    _create_history_permission(user, model)
+    _type = ContentType.objects.get_for_model(model).model
+    obj = _create_few_models(factory)
+    hist_obj = obj.history.first()
+    history_url = f'/api/v{options["version"]}/{_type}-list/history/'
+
+    code, content = _do_async_fake_request(
+        user.pk, 'get', history_url, data={'history_id': hist_obj.history_id})
+
+    assert code == 200
+
+    content_json = json.loads(content)
+    assert content_json['count'] == 1
+    assert content_json['results'][0]["history_type"] == '+'
+    assert content_json['results'][0]["history_id"] == hist_obj.history_id
+    assert content_json['results'][0]["_uid"] == str(hist_obj.uid)
+    assert content_json['results'][0]["_type"] == 'historical' + _type
+
+
+def test_replicate_history_call_process_webhook(
+        api_model, mocker, celery_session_worker):
+    model, factory, options = api_model
+    subscribe = _create_subscribe(model, options)
+    process_webhook = mocker.patch(
+        'replication.tasks._replicate_history_process_webhook_subscriber')
+
+    # create event
+    obj = factory.create()
+    history = obj.history.all()
+    hist1 = history.first()
+    app_label, model_name = hist1._meta.app_label, hist1._meta.model_name
+
+    r = replicate_history.delay(app_label, model_name, hist1.pk)
+    r.get(timeout=10)
+    assert process_webhook.delay.call_args_list == [
+        call(subscribe.pk, app_label, model_name, hist1.pk),
+        call(subscribe.pk, app_label, model_name, hist1.pk),
+    ]
+
+    # change event
+    obj.version += 10
+    obj.save()
+    hist2 = history.first()
+
+    r = replicate_history.delay(app_label, model_name, hist2.pk)
+    r.get(timeout=10)
+    assert process_webhook.delay.call_args_list == [
+        call(subscribe.pk, app_label, model_name, hist1.pk),
+        call(subscribe.pk, app_label, model_name, hist1.pk),
+        call(subscribe.pk, app_label, model_name, hist2.pk),
+        call(subscribe.pk, app_label, model_name, hist2.pk),
+    ]
+
+    # delete event
+    obj.delete()
+    hist3 = history.first()
+
+    r = replicate_history.delay(app_label, model_name, hist3.pk)
+    r.get(timeout=10)
+    assert process_webhook.delay.call_args_list == [
+        call(subscribe.pk, app_label, model_name, hist1.pk),
+        call(subscribe.pk, app_label, model_name, hist1.pk),
+        call(subscribe.pk, app_label, model_name, hist2.pk),
+        call(subscribe.pk, app_label, model_name, hist2.pk),
+        call(subscribe.pk, app_label, model_name, hist3.pk),
+        call(subscribe.pk, app_label, model_name, hist3.pk),
+    ]
+
+
+def test_process_webhook(api_model, mocker, celery_session_worker):
+    model, factory, options = api_model
+    subscribe = _create_subscribe(model, options)
+    process_webhook = _replicate_history_process_webhook_subscriber
+    transfer = mocker.patch('replication.tasks._transfer_webhook_data')
+
+    # create event
+    obj = _create_few_models(factory)
+    history = obj.history.all()
+    hist1 = history.first()
+    hist1_content = _get_history_content(model, options, subscribe.user, hist1)
+    app_label, model_name = hist1._meta.app_label, hist1._meta.model_name
+
+    r = process_webhook.delay(subscribe.pk, app_label, model_name, hist1.pk)
+    assert r.get(timeout=10) == 'ok'
+    assert transfer.call_args_list == [
+        call('http://localhost/', hist1_content),
+        call('http://localhost/', hist1_content),
+    ]
+
 
 def test_process_webhook_retry(api_model, celery_session_worker):
     model, factory, options = api_model
