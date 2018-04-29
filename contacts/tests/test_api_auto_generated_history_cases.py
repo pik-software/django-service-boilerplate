3d85b9181a8372503d980a7ce1bdc95b46fbe38e
Pahaz White <pahaz.white@gmail.com>
2018-04-29 13:48:35 +0500

    contacts: refactor tests

---
+++ contacts/tests/test_api_auto_generated_history_cases.py
@@ -0,0 +1,116 @@
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.tasks.fixtures import create_user
+from ..models import Contact, Comment
+from ..tests.factories import ContactFactory, CommentFactory
+
+
+BATCH_MODELS = 5
+
+
+@pytest.fixture(params=[
+    (Contact, ContactFactory, {'version': 1}),
+    (Comment, CommentFactory, {'version': 1}),
+])
+def api_model(request):
+    return request.param
+
+
+@pytest.fixture()
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def api_client():
+    user = create_user()
+    client = APIClient()
+    client.force_login(user)
+    client.user = user
+    return client
+
+
+def _url(model, options):
+    _type = ContentType.objects.get_for_model(model).model
+    return f'/api/v{options["version"]}/{_type}-list/history/'
+
+
+def _create_few_models(factory):
+    factory.create_batch(BATCH_MODELS)
+    last_obj = factory.create()
+    return last_obj
+
+
+def _create_history_permission(user, model):
+    model_type_name = ContentType.objects.get_for_model(model).model
+    permission = Permission.objects.get(
+        codename=f'can_get_api_{model_type_name}_history')
+    user.user_permissions.add(permission)
+
+
+def test_api_history_access_denied(api_client, api_model):
+    model, factory, options = api_model
+    _create_few_models(factory)
+    url = _url(model, options)
+
+    res = api_client.get(url)
+
+    assert res.status_code == status.HTTP_403_FORBIDDEN
+    assert res.data == {
+        'code': 'permission_denied', 'message': 'Access denied'}
+
+
+def test_api_history(api_client, api_model):
+    model, factory, options = api_model
+    _create_few_models(factory)
+    url = _url(model, options)
+
+    _create_history_permission(api_client.user, model)
+    res = api_client.get(url)
+
+    assert res.status_code == status.HTTP_200_OK
+    assert res.data['count'] == BATCH_MODELS + 1
+
+
+def test_api_history_filter_by_uid(api_client, api_model):
+    model, factory, options = api_model
+    last_obj = _create_few_models(factory)
+    url = _url(model, options)
+    _type = ContentType.objects.get_for_model(model).model
+
+    _create_history_permission(api_client.user, model)
+    res = api_client.get(f'{url}?_uid={last_obj.uid}')
+
+    assert res.status_code == status.HTTP_200_OK
+    count = res.data['count']
+    first_result = res.data['results'][0]
+    assert count == 1
+    assert first_result['_uid'] == last_obj.uid
+    assert first_result['_type'] == 'historical' + _type
+    assert first_result['history_change_reason'] is None
+    assert first_result['history_type'] == "+"
+
+
+def test_api_history_create_then_change(api_client, api_model):
+    model, factory, options = api_model
+    last_obj = _create_few_models(factory)
+    url = _url(model, options)
+
+    _create_history_permission(api_client.user, model)
+    last_obj.save()
+    res = api_client.get(f'{url}?_uid={last_obj.uid}')
+
+    assert res.status_code == status.HTTP_200_OK
+    assert res.data['count'] == 2
+    first_result = res.data['results'][0]
+    assert first_result['_uid'] == last_obj.uid
+    assert first_result['history_change_reason'] is None
+    assert first_result['history_type'] == "~"
+    second_result = res.data['results'][1]
+    assert second_result['_uid'] == last_obj.uid
+    assert second_result['history_change_reason'] is None
+    assert second_result['history_type'] == "+"
---
+++ contacts/tests/test_api_v1_read.py
@@ -0,0 +1,59 @@
+from pprint import pprint
+
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.tasks.fixtures import create_user
+from ..models import Contact
+from ..tests.factories import ContactFactory
+
+
+@pytest.fixture
+def api_client():
+    user = create_user()
+    client = APIClient()
+    client.force_login(user)
+    client.user = user
+    return client
+
+
+def _assert_api_results(data, result):
+    data = list(map(dict, data['results']))
+    pprint(data)
+    assert data == result
+
+
+def _assert_api_object(data, result):
+    pprint(data)
+    assert data == result
+
+
+def test_api_list_contact(api_client):  # noqa: pylint=invalid-name
+    obj = ContactFactory.create()
+    res = api_client.get('/api/v1/contact-list/')
+    assert res.status_code == status.HTTP_200_OK
+    _assert_api_results(res.data, [
+        {
+            '_type': 'contact',
+            '_uid': obj.uid,
+            'emails': obj.emails,
+            'name': obj.name,
+            'order_index': obj.order_index,
+            'phones': obj.phones,
+        }
+    ])
+
+
+def test_api_retrieve_contact(api_client):
+    obj: Contact = ContactFactory.create()
+    res = api_client.get(f'/api/v1/contact-list/{obj.uid}/')
+    assert res.status_code == status.HTTP_200_OK
+    _assert_api_object(res.data, {
+        '_type': 'contact',
+        '_uid': obj.uid,
+        'emails': obj.emails,
+        'name': obj.name,
+        'order_index': obj.order_index,
+        'phones': obj.phones,
+    })
