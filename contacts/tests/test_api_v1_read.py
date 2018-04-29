22b9e3a6ece69634ce98db8a671bcaaa78d66017
Pavel White <pahaz.white@gmail.com>
2018-04-29 14:52:37 +0500

    Update test_api_v1_read.py

--- contacts/tests/test_api_v1_read.py
+++ contacts/tests/test_api_v1_read.py
@@ -5,7 +5,6 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from core.tasks.fixtures import create_user
-from ..models import Contact
 from ..tests.factories import ContactFactory
 
 
