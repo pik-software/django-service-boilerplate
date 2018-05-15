d3f5f971b7309403ab6c3637733a70007935215e
Pahaz White <pahaz.white@gmail.com>
2018-05-15 19:14:42 +0300

    test_celery: clear existed tasks

--- _project_/tests/test_celery.py
+++ _project_/tests/test_celery.py
@@ -1,7 +1,8 @@
 from _project_.celery import debug_task
 
 
-def test_create_task(celery_worker):
+def test_create_task(celery_session_worker):
+    celery_session_worker.app.control.purge()  # clear all existed tasks
     req = debug_task.delay()
     assert req.status in ['PENDING', 'SUCCESS']
     assert req.get(timeout=10) == "Request: '_project_.celery.debug_task'"
