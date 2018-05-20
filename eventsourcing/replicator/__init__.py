a52351318fe60c7929f579ef743c20fc6f62384f
Pahaz White <pahaz.white@gmail.com>
2018-05-20 16:51:34 +0300

    prospector

--- eventsourcing/replicator/__init__.py
+++ eventsourcing/replicator/__init__.py
@@ -1,6 +1,7 @@
 from .api.viewsets import SubscriptionViewSet
 from .registry import replicating, is_replicating, replicate
 from .serializer import serialize
+from .tasks import _replicate_to_webhook_subscribers
 
 __all__ = [
     'replicating', 'is_replicating', 'serialize', 'replicate',
