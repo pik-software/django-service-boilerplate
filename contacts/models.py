a6c6219d2713d5531d2649a95f30b1bde4860a52
martbln <stepinilya@gmail.com>
2018-06-05 14:04:09 +0300

    contacts: update models from pik_django_utils

--- contacts/models.py
+++ contacts/models.py
@@ -1,12 +1,10 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from simple_history.models import HistoricalRecords
-
-from core.models import Versioned, Uided, Dated, PUided, Owned
+from pik.core.models import BaseHistorical, BasePHistorical, Owned
 
 
-class Contact(Uided, Dated, Versioned):
+class Contact(BaseHistorical):
     UID_PREFIX = 'CON'
     name = models.CharField(_('Наименование'), max_length=255)
     phones = ArrayField(
@@ -22,8 +20,6 @@ class Contact(Uided, Dated, Versioned):
 
     order_index = models.IntegerField(_('Индекс для сортировки'), default=100)
 
-    history = HistoricalRecords()
-
     def __str__(self):
         return f'{self.name}'
 
@@ -36,14 +32,12 @@ class Contact(Uided, Dated, Versioned):
         )
 
 
-class Comment(PUided, Dated, Versioned, Owned):
+class Comment(BasePHistorical, Owned):
     UID_PREFIX = 'COM'
     contact = models.ForeignKey(
         Contact, related_name='comments',
         on_delete=models.CASCADE)
     message = models.TextField(_('Сообщение'))
-
-    history = HistoricalRecords()
 
     def __str__(self):
         return f'{self.user}: {self.message}'
