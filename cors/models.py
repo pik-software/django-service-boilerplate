112cb8b7fddac8babed55ad5a0555b4aa6da5633
Alexander Klimenko <alex@erix.ru>
2018-07-17 14:42:09 +0300

    cors: add help_text

--- cors/models.py
+++ cors/models.py
@@ -4,7 +4,9 @@ from pik.core.models import BasePHistorical
 
 
 class Cors(BasePHistorical):
-    cors = models.CharField(max_length=255, unique=True)
+    cors = models.CharField(max_length=255, unique=True,help_text=_(
+        "Название домена допущенного делать междоменные запросы, например: "
+        "staff-front.pik-software.ru или localhost:3000"))
 
     def __str__(self):
         return self.cors
