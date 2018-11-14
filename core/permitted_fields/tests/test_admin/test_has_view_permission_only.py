616f8db1e106c1ccc59cdd89609323cd7d978f2a
Alexander Klimenko <alex@erix.ru>
2018-11-14 11:29:10 +0300

    core.permitted_fields.test: fix test skip

--- core/permitted_fields/tests/test_admin/test_has_view_permission_only.py
+++ core/permitted_fields/tests/test_admin/test_has_view_permission_only.py
@@ -1,23 +1,8 @@
 from unittest.mock import Mock
 
-import pytest
-
 from core.permitted_fields.admin import PermittedFieldsAdminMixIn
 
-try:
-    import admin_view_permission  # noqa unused-import
-except ImportError:
-    VIEW_PERMISSION = False
-else:
-    VIEW_PERMISSION = True
 
-
-skip_admin_view_permission_missing = pytest.mark.skipif(  # noqa
-    not VIEW_PERMISSION,
-    reason='Django Admin View permission is missing')
-
-
-@skip_admin_view_permission_missing
 def test_obj_view_only():
     admin = PermittedFieldsAdminMixIn()
     admin.has_view_permission = Mock(return_value=True)
@@ -26,7 +11,6 @@ def test_obj_view_only():
     assert admin._has_view_permission_only(request=Mock(), obj=Mock())  # noqa  protected-access
 
 
-@skip_admin_view_permission_missing
 def test_obj_view_and_change():
     admin = PermittedFieldsAdminMixIn()
     admin.has_view_permission = Mock(return_value=True)
@@ -35,7 +19,6 @@ def test_obj_view_and_change():
     assert not admin._has_view_permission_only(request=Mock(), obj=Mock())  # noqa  protected-access
 
 
-@skip_admin_view_permission_missing
 def test_missing_obj_add_permission():
     admin = PermittedFieldsAdminMixIn()
     admin.has_add_permission = Mock(return_value=True)
@@ -43,7 +26,6 @@ def test_missing_obj_add_permission():
     assert not admin._has_view_permission_only(request=Mock(), obj=None)  # noqa  protected-access
 
 
-@skip_admin_view_permission_missing
 def test_missing_obj_add_permission_missing(): # noqa: invalid-name
     admin = PermittedFieldsAdminMixIn()
     admin.has_add_permission = Mock(return_value=False)
