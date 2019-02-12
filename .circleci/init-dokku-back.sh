721d50de344d09aa270e83dc61e7c697a8052b4e
Pahaz White <pahaz.white@gmail.com>
2019-02-12 17:19:43 +0300

    init-dokku-back.sh: need sudo for mkdir by circleci user

--- .circleci/init-dokku-back.sh
+++ .circleci/init-dokku-back.sh
@@ -21,8 +21,8 @@ if ssh dokku@${SSH_HOST} -C apps:list | grep -qFx ${SERVICE_NAME}; then
     exit 2
 fi
 
+ssh ${SSH_HOST} -C sudo mkdir "${MEDIA_ROOT}"
 ssh ${SSH_HOST} -C dokku apps:create ${SERVICE_NAME}
-ssh ${SSH_HOST} -C mkdir "${MEDIA_ROOT}"
 ssh dokku@${SSH_HOST} -C storage:mount ${SERVICE_NAME} "${MEDIA_ROOT}:${MEDIA_ROOT}"
 ssh dokku@${SSH_HOST} -C domains:set ${SERVICE_NAME} ${SERVICE_HOST}
 
