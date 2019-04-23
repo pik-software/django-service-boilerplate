a4bbe82889f3018ed56f10c1259a0d8033a936e7
Pahaz White <pahaz.white@gmail.com>
2019-04-23 19:44:04 +0300

    run_celery_worker.sh: remove CELERY_WORKER_CONCURRENCY

--- _bin/run_celery_worker.sh
+++ _bin/run_celery_worker.sh
@@ -4,7 +4,7 @@ if [ "$ENVIRONMENT" = "staging" ]
 then
   concurrency=1
 else
-  concurrency=${2:-$CELERY_WORKER_CONCURRENCY}
+  concurrency=${2:-2}
 fi
 queue_name=${1:-celery}
 exec python -u -m celery worker -A _project_ --loglevel info -Q $queue_name -c $concurrency
