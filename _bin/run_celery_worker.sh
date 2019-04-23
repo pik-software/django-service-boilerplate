e9b7e211662a1fb5053d03236ec87408d1768fab
Pahaz White <pahaz.white@gmail.com>
2019-04-23 19:20:24 +0300

    run_celery_worker.sh: priorities and right settings value

--- _bin/run_celery_worker.sh
+++ _bin/run_celery_worker.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if [ "$ENVIRONMENT" = "staging" ]
-then
-  concurrency=1
-else
-  concurrency=${2:-$CELERY_WORKER_CONCURRENCY}
-fi
-queue_name=${1:-celery}
+# defaults
+case "$ENVIRONMENT" in
+ staging) default_concurrency=1 ;;
+ *) default_concurrency=2 ;;
+esac
+default_queue_name=celery
+
+# args -> env -> default
+queue_name=${1:-${CELERY_WORKER_QUEUE_NAME:-$default_queue_name}}
+concurrency=${2:-${CELERY_WORKER_CONCURRENCY:-$default_concurrency}}
+
+export CELERY_WORKER_QUEUE_NAME="${queue_name}"
+export CELERY_WORKER_CONCURRENCY="${concurrency}"
 exec python -u -m celery worker -A _project_ --loglevel info -Q $queue_name -c $concurrency
