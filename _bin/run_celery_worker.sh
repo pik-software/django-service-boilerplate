5dd2fe1ba8eed1a2bbf5ce68fa4512dda9d8b2e5
Greg Eremeev <gregory.eremeev@gmail.com>
2019-04-23 17:05:31 +0300

    Add celery concurrency default value when staging environment (#301)

--- _bin/run_celery_worker.sh
+++ _bin/run_celery_worker.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-exec python -u -m celery worker -A _project_ --loglevel info
+if [ "$ENVIRONMENT" = "staging" ]
+then
+  concurrency=1
+else
+  concurrency=${2:-$CELERY_WORKER_CONCURRENCY}
+fi
+queue_name=${1:-celery}
+exec python -u -m celery worker -A _project_ --loglevel info -Q $queue_name -c $concurrency
