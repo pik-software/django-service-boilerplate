33fcf09956d05aeefe7fc0f17d274153feb04485
MaximZemskov <m.zemskov1@gmail.com>
2019-04-24 12:24:15 +0300

    core.fields: NormalizedCharField

--- core/fields.py
+++ core/fields.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from pik.utils.normalization import normalize
 
 MAX_DIGITS = 16
 DECIMAL_PLACES = 2
@@ -11,3 +13,13 @@ class MoneyField(models.DecimalField):
         super().__init__(
             *args, max_digits=max_digits,
             decimal_places=decimal_places, **kwargs)
+
+
+class NormalizedCharField(models.CharField):
+
+    description = _('Normalized by pik.utils.normalization.normalize')
+
+    def pre_save(self, model_instance, add):
+        value = normalize(getattr(model_instance, self.attname))
+        setattr(model_instance, self.attname, value)
+        return value
