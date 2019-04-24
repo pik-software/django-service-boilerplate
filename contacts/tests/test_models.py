92056b1c3431ed3ce5253b4e2dcaceaec57183b6
MaximZemskov <m.zemskov1@gmail.com>
2019-04-24 13:40:11 +0300

    contacts.tests: remove 0 width charcters from test

--- contacts/tests/test_models.py
+++ contacts/tests/test_models.py
@@ -61,8 +61,7 @@ def test_increment_version(critical_model_and_factory):
 space_unicodes = [  # noqa: invalid name
     '\xa0', '\u1680', '\u2000', '\u2001', '\u2002', '\u2003',
     '\u2004', '\u2005', '\u2006', '\u2007', '\u2008',
-    '\u2009', '\u200A', '\u200B', '\u202F', '\u205F',
-    '\u3000', '\uFEFF']
+    '\u2009', '\u200A', '\u202F', '\u205F', '\u3000']
 
 
 @pytest.mark.parametrize('space_unicode', space_unicodes)
