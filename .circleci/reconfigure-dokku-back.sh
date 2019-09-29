2eab79860848b91bf4e8e92fb28242fed3144126
Pahaz White <pahaz.white@gmail.com>
2019-09-29 12:18:44 +0300

    circleci: remove SENTRY and LETSENCRYPT hardcoded values

--- .circleci/reconfigure-dokku-back.sh
+++ .circleci/reconfigure-dokku-back.sh
@@ -10,7 +10,6 @@ SERVICE_HOST=$2
 SERVICE_NAME=$3
 BRANCH=$4
 ENVIRONMENT=$5
-SENTRY_TEAM='nsi'
 
 if [[ -z "${SSH_HOST}" ]] || [[ -z "${SERVICE_HOST}" ]] || [[ -z "${SERVICE_NAME}" ]] || [[ -z "${BRANCH}" ]] || [[ -z "${ENVIRONMENT}" ]]; then
     echo "Use: $0 <SSH_HOST> <SERVICE_HOST> <SERVICE_NAME> <BRANCH> <ENVIRONMENT>"
@@ -18,7 +17,7 @@ if [[ -z "${SSH_HOST}" ]] || [[ -z "${SERVICE_HOST}" ]] || [[ -z "${SERVICE_NAME
 fi
 
 RELEASE_DATE=$( date '+%Y-%m-%d-%H-%M-%S' )
-RELEASE=`git describe --tags --match v[0-9]*`
+RELEASE=$(git describe --tags --match v[0-9]*)
 ssh dokku@${SSH_HOST} -C config:set --no-restart ${SERVICE_NAME} RELEASE_DATE="'"${RELEASE_DATE}"'" SENTRY_RELEASE=${RELEASE}
 GIT_REV=$(git rev-parse ${BRANCH})
 ssh dokku@${SSH_HOST} -C config:set --no-restart ${SERVICE_NAME} GIT_REV=${GIT_REV}
@@ -27,14 +26,17 @@ ssh dokku@${SSH_HOST} -C config:set --no-restart ${SERVICE_NAME} GIT_REV=${GIT_R
 case "$ENVIRONMENT" in
     production)
         ssh dokku@${SSH_HOST} -C config:set --no-restart ${SERVICE_NAME} EXAMPLE=1
-        SENTRY_URL='https://sentry.pik-software.ru/api/'
         ;;
     staging)
         ssh dokku@${SSH_HOST} -C config:set --no-restart ${SERVICE_NAME} EXAMPLE=2
         ssh dokku@${SSH_HOST} -C config:set --no-restart ${SERVICE_NAME} BRANCH=${BRANCH}
-        SENTRY_URL='https://sentry-staging.pik-software.ru/api/'
         ;;
 esac
 
-SENTRY_DSN=`python3 get-sentry-dsn.py -a ${SENTRY_URL} -p ${SERVICE_NAME} -t ${SENTRY_TEAM} -k ${SENTRY_API_KEY}`
-ssh dokku@${SSH_HOST} -C config:set --no-restart ${SERVICE_NAME} SENTRY_DSN=${SENTRY_DSN}
+if [[ -n "${SENTRY_URL}" && -n "${SENTRY_TEAM}" && -n "${SENTRY_API_KEY}" ]]; then
+    echo "Run SENTRY DSN discovery"
+    SENTRY_DSN=$(python3 get-sentry-dsn.py -a "${SENTRY_URL}" -p "${SERVICE_NAME}" -t "${SENTRY_TEAM}" -k "${SENTRY_API_KEY}")
+    ssh dokku@${SSH_HOST} -C config:set --no-restart ${SERVICE_NAME} SENTRY_DSN=${SENTRY_DSN}
+else
+    echo "No SENTRY DSN discovery settings (skip)"
+fi
