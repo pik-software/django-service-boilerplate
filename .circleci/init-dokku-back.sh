532e8e1beeb39c633bbf9b54232cfb14546450c1
martbln <stepinilya@gmail.com>
2019-12-11 12:16:43 +0300

    create celerybeat dir

--- .circleci/init-dokku-back.sh
+++ .circleci/init-dokku-back.sh
@@ -37,9 +37,9 @@ ssh dokku@${SSH_HOST} -C redis:create ${SERVICE_NAME}
 ssh dokku@${SSH_HOST} -C redis:link ${SERVICE_NAME} ${SERVICE_NAME}
 
 # celery beat schedule
-ssh ${SSH_HOST} -C sudo mkdir -p "${CELERYBEAT_SCHEDULER_DIR}"
+ssh ${SSH_HOST} -C mkdir -p "${CELERYBEAT_SCHEDULER_DIR}"
 ssh ${SSH_HOST} -C chown -R dokku:dokku "${CELERYBEAT_SCHEDULER_DIR}"
-ssh dokku${SSH_HOST} -C storage:mount ${SERVICE_NAME} "${CELERYBEAT_SCHEDULER_DIR}:/tmp"
+ssh dokku@${SSH_HOST} -C storage:mount ${SERVICE_NAME} "${CELERYBEAT_SCHEDULER_DIR}:/tmp"
 
 # dd-agent
 if ssh ${SSH_HOST} -C docker ps | grep -q dd-agent; then
--- .circleci/reconfigure-dokku-back.sh
+++ .circleci/reconfigure-dokku-back.sh
@@ -10,6 +10,7 @@ SERVICE_HOST=$2
 SERVICE_NAME=$3
 BRANCH=$4
 ENVIRONMENT=$5
+CELERYBEAT_SCHEDULER_DIR=/tmp/CELERYBEAT_SCHEDULER/
 
 if [[ -z "${SSH_HOST}" ]] || [[ -z "${SERVICE_HOST}" ]] || [[ -z "${SERVICE_NAME}" ]] || [[ -z "${BRANCH}" ]] || [[ -z "${ENVIRONMENT}" ]]; then
     echo "Use: $0 <SSH_HOST> <SERVICE_HOST> <SERVICE_NAME> <BRANCH> <ENVIRONMENT>"
@@ -39,4 +40,17 @@ if [[ -n "${SENTRY_URL}" && -n "${SENTRY_TEAM}" && -n "${SENTRY_API_KEY}" ]]; th
     ssh dokku@${SSH_HOST} -C config:set --no-restart ${SERVICE_NAME} SENTRY_DSN=${SENTRY_DSN}
 else
     echo "No SENTRY DSN discovery settings (skip)"
+fi
+
+if [ ! -d "$CELERYBEAT_SCHEDULER_DIR" ]; then
+  echo "Create CELERYBEAT Schedule dir"
+
+  echo "mkdir"
+  ssh ${SSH_HOST} -C mkdir -p ${CELERYBEAT_SCHEDULER_DIR}
+
+#  echo "chown"
+#  ssh ${SSH_HOST} -C chown -R dokku:dokku ${CELERYBEAT_SCHEDULER_DIR}
+
+  echo "dokku mount"
+  ssh dokku@${SSH_HOST} -C storage:mount ${SERVICE_NAME} "${CELERYBEAT_SCHEDULER_DIR}:/tmp"
 fi
