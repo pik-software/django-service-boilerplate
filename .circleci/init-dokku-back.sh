818ea0e6702d985c0ba3b13499764166910d2685
martbln <stepinilya@gmail.com>
2019-12-12 15:56:04 +0300

    fix

--- .circleci/init-dokku-back.sh
+++ .circleci/init-dokku-back.sh
@@ -37,8 +37,8 @@ ssh dokku@${SSH_HOST} -C redis:create ${SERVICE_NAME}
 ssh dokku@${SSH_HOST} -C redis:link ${SERVICE_NAME} ${SERVICE_NAME}
 
 # celery beat schedule
-#ssh ${SSH_HOST} -C mkdir -p "${CELERYBEAT_SCHEDULER_DIR}"
-#ssh dokku@${SSH_HOST} -C storage:mount ${SERVICE_NAME} "${CELERYBEAT_SCHEDULER_DIR}:/tmp"
+ssh ${SSH_HOST} -C mkdir -p "${CELERYBEAT_SCHEDULER_DIR}"
+ssh dokku@${SSH_HOST} -C storage:mount ${SERVICE_NAME} "${CELERYBEAT_SCHEDULER_DIR}:/tmp"
 
 # dd-agent
 if ssh ${SSH_HOST} -C docker ps | grep -q dd-agent; then
