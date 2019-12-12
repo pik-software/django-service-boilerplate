71ebfcfc5baf419943a4ed370b39bf557de1ba8c
martbln <stepinilya@gmail.com>
2019-12-12 11:20:03 +0300

    fix

--- _bin/run_celery_beat.sh
+++ _bin/run_celery_beat.sh
@@ -2,4 +2,4 @@
 
 CELERY_BEAT_PID_PATH=/tmp/celerybeat.pid
 rm -f $CELERY_BEAT_PID_PATH
-exec python -u -m celery beat -A _project_ --loglevel info --schedule="/tmp/celerybeat-schedule.db"
+exec python -u -m celery beat -A _project_ --loglevel info --pidfile=$CELERY_BEAT_PID_PATH --schedule="/tmp/celerybeat-schedule.db"
